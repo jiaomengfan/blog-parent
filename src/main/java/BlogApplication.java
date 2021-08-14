8883f5d7d7b1aa29c360ba68687e56400ebe8418
焦梦凡 <jiaomf6059@163.com>
2021-08-14 13:17:26 +0800

    目的：博客系统 1.新建启动类 反馈：可以运行

---
+++ src/main/java/BlogApplication.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName BlogApplication
+ * @Description TODO
+ * @Author jiaomf
+ * @Date 2021/8/14 13:07
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class,args);
+    }
+}
